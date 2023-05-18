c7c86aa399fbe2ca2168866ef97abb7287473dd2
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-05-18 11:17:46 +0100

    Add House class;

---
+++ house.rb
@@ -0,0 +1,8 @@
+class House
+    def initialize(bathroom, living_room, bedroom, kitchen)
+        @bathroom = bathroom
+        @living_room = living_room
+        @bedroom = bedroom
+        @kitchen = kitchen
+    end
+end
